a94fe4278d71b61c25946eb0849e5eb88f83da51
Amirreza Madanifard <amadanifard@gmail.com>
2024-10-07 00:23:35 +0330

    chnage url swager

--- insurance/urls.py
+++ insurance/urls.py
@@ -9,15 +9,15 @@ urlpatterns = [
          FullDataAPIView.as_view(),
          name='full-data-api'),
 
-    path('api/schema',
+    path('schema',
          SpectacularAPIView.as_view(),
          name='schema'),
 
-    path('api/schema/swagger-ui',
+    path('schema/swagger-ui',
          SpectacularSwaggerView.as_view(url_name='schema'),
          name='swagger-ui'),
 
-    path('api/schema/redoc',
+    path('schema/redoc',
          SpectacularRedocView.as_view(url_name='schema'),
          name='redoc'),
 ]
