c46fd1d27a55bdb4f5ea658437c88d85edd26c70
Amirreza Madanifard <amadanifard@gmail.com>
2024-10-06 19:04:19 +0330

    insurance app config

--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',  # setting ==> REST_FRAMEWORK
     'drf_spectacular',  # settings ==> SPECTACULAR_SETTINGS
+    'insurance',
 ]
 
 MIDDLEWARE = [
